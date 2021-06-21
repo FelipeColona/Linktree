6d6b2b638691671a5fbfa91b48f13101f8c090ae
FelipeColona <72169708+FelipeColona@users.noreply.github.com>
2021-06-21 18:42:00 -0300

    feat: add delete method to shorturl controller
    Implemented it using raw sql

--- src/main/java/br/com/linktree/api/controllers/ShorturlController.java
+++ src/main/java/br/com/linktree/api/controllers/ShorturlController.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import br.com.linktree.domain.model.Shorturl;
+import br.com.linktree.repository.LinkRepository;
 import br.com.linktree.repository.ShorturlRepository;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +29,9 @@ public class ShorturlController {
     @Autowired
     private ShorturlRepository shorturlRepository;
 
+    @Autowired
+    private LinkRepository linkRepository;
+
     @GetMapping
     public List<Shorturl> list(){
         return shorturlRepository.findAll(); 
@@ -32,12 +39,21 @@ public class ShorturlController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    private Shorturl create(){
+    public Shorturl create(){
         Shorturl shorturl = new Shorturl();
         String uuid = this.genUUID();
         shorturl.setId(uuid);
 
         return shorturlRepository.save(shorturl);
+    }
+
+    @DeleteMapping("/{shorturlId}")
+    public ResponseEntity<Void> exclude(@PathVariable String shorturlId){
+        linkRepository.deleteByShorturl_id(shorturlId);
+
+        shorturlRepository.deleteByShorturl_id(shorturlId);
+
+        return ResponseEntity.noContent().build();   
     }
 
     private String genUUID(){
--- src/main/java/br/com/linktree/repository/ShorturlRepository.java
+++ src/main/java/br/com/linktree/repository/ShorturlRepository.java
@@ -3,9 +3,20 @@ package br.com.linktree.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.linktree.domain.model.Shorturl;
 
 public interface ShorturlRepository extends JpaRepository<Shorturl, Long> {
     Optional<Shorturl> findById(String uuid);
+    
+    @Modifying
+    @Transactional
+    @Query(
+        value = "DELETE FROM shorturl WHERE id = ?1", 
+        nativeQuery = true
+    )
+    Integer deleteByShorturl_id(String uuid);
 }
